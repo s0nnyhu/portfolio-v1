e7e0bafb9136f57c59eb8c6180bed7b375871d6a
Sonny Hu <sonny.hu@toulouse.miage.fr>
2019-11-13 16:40:17 +0100

    WIP app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          s0nnyhu github page made using <code>React </code> [Extremely WIP]
         </p>
         <a
           className="App-link"
