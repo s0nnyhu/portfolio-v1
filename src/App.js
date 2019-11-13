4302b044553b63228401a1d3a0b4f18ea1e323e8
Sonny Hu <sonny.hu@toulouse.miage.fr>
2019-11-13 17:17:10 +0100

    Currently in dev index.html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          s0nnyhu github page made using <code>React </code> [Extremely WIP]
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>
+          Currently in development
+        </h2>
+        <h2>
+          ...or maybe not actually
+        </h2>
       </header>
     </div>
   );
